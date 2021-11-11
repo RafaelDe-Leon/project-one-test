e871c1d9cf73971eaec8e6f90cf1713ae4595b15
Rafael De Leon <rafael@ridewithloop.com>
2021-11-10 19:48:09 -0500

    fixed fetch

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 //   .then(data => console.log(data));
 
 fetch(
-  'https://gateway-cagi34uoda-uc.a.run.app/credit_score_impact/500?origin=homepage'
+  'https://gateway-rjebnrx5ja-uc.a.run.app/credit_score_impact/500?origin=homepage'
 )
   .then(res => res.json())
   .then(data => console.log(data));
