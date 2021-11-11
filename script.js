a03d107962c49efcdf01eebc1e2c44e6495d4aae
Rafael De Leon <rafael@ridewithloop.com>
2021-11-11 13:29:41 -0500

    testing for deployed

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 //   .then(data => console.log(data));
 
 fetch(
-  'https://gateway-rjebnrx5ja-uc.a.run.app/credit_score_impact/500?origin=homepage'
+  'https://gateway-cagi34uoda-uc.a.run.app/credit_score_impact/500?origin=homepage'
 )
   .then(res => res.json())
   .then(data => console.log(data));
+
+// cors = CORS(app, (origins = 'https://www.ridewithloop.com'));
