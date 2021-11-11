bf40cad809973c82894002dd394ae3b0a83e81d0
Rafael De Leon <rafael@ridewithloop.com>
2021-11-10 20:57:40 -0500

    test

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 //   .then(res => res.json())
 //   .then(data => console.log(data));
 
-fetch('https://gateway-rjebnrx5ja-uc.a.run.app/credit_score_impact/500')
+fetch(
+  'https://gateway-rjebnrx5ja-uc.a.run.app/credit_score_impact/500?origin=homepage'
+)
   .then(res => res.json())
   .then(data => console.log(data));
